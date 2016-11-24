f252729112323f7a8c49583bac64dcfbcad322e5
section-1 <section-1@DESKTOP-93G7NTJ>
2016-11-24 13:03:02 +0300

    Added Project

--- Program.cs
+++ Program.cs
@@ -16,7 +16,6 @@ namespace SayindanKelimeye.WFA
         static void Main()
         {
             Application.EnableVisualStyles();
-
             Application.SetCompatibleTextRenderingDefault(false);
 
             Application.Run(new Form1());
